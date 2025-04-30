66d66b913e091a0af302eae397122a7746dcbeac
chdahao <chendehang@live.com>
2025-04-30 22:26:20 +0800

    tag 1.1.0

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
             path: "AGGeometryKit",
             publicHeadersPath: "include",
             cSettings: [
+                .headerSearchPath("Classes"),
+                .headerSearchPath("Categories"),
+                .headerSearchPath("CoreGraphics_Extensions"),
                 .headerSearchPath(".")
             ]
         )
